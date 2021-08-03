13cb1acc8af1d4f7bea3640981e1acf3e3ff2bf4
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-08-03 15:04:34 -0700

    added auth0, no formatting yet

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+
+
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  // if (isLoading) {
+  //   return <div>Loading ...</div>;
+  // }
+
+  return (
+    isAuthenticated && (
+      <div className = "profile">
+        <img src={user.picture} alt={user.name} />
+        <h2>Name: {user.name}</h2>
+        <p>Email: {user.email}</p>
+        {/* <JSONPretty data = {user} /> */}
+      </div>
+    )
+   
+  );
+};
+
+export default Profile;
