01d7a6341f6e1f7b91252a7dcf374ffa066551cc
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-08-05 17:17:00 -0700

    search bar navbar styling and login button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MenuItems from './MenuItems.js';
 import './Navbar.css';
 import LoginButton from '../LoginButton.js';
 import LogoutButton from '../LogoutButton.js';
@@ -17,21 +16,10 @@ class Navbar extends React.Component {
     return (
       <nav className = "NavbarItems">
         <h1 className = "navbar-logo" >Archi-Trek <i className ="fas fa-mountain"></i></h1>
-        {/* <h1 className = "navbar-logo" >Archi-Trek <img id = "mountain" src = "white-mountain.png" alt=""></img></h1> */}
         <div className = "menu-icon" onClick={this.handleClick}>
           <i className = {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
         <ul className = {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key = {index}>
-                <a id = "links" className = {item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            )
-          })}
-
         </ul>
 
         <div id = "loginButton">
